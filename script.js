afb2b37c9a61a6bb2397ad54d759aa7479678048
michaLewicki <47480972+michaLewicki@users.noreply.github.com>
2024-08-10 19:10:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+window.onload = function()
+{
+    var input = document.getElementById("tekst");
+    var przycisk_oblicz = document.querySelector("button[class=oblicz]");
+	var przycisk_kopiuj = document.querySelector("button[class=kopiuj]");
+    var wynik = document.getElementById("wynik");
+
+    var tekst = "";
+    przycisk_kopiuj.disabled = true;
+
+	przycisk_oblicz.onclick = function()
+	{
+        wynik.value = "";
+        tekst = input.value;
+        if(tekst.length === 0) 
+		{
+			przycisk_kopiuj.disabled = true;
+			return false;
+		}
+		tekst = tekst.replaceAll("Dz. Ap.", "Dzieje");
+		tekst = tekst.replaceAll("PnP", "Pieśń");
+		tekst = tekst.replaceAll("Kaz. Sal.", "Kazn.");
+		tekst = tekst.replaceAll("Mich.", "Micheasza");
+		tekst = tekst.replaceAll("Mar.", "Marka");
+		tekst = tekst.replaceAll("Fil.", "Filip.");
+		tekst = tekst.replaceAll("Juda", "Judy");
+		tekst = tekst.replaceAll("Abak.", "Habak.");
+		tekst = tekst.replaceAll("Ruty", "Rut");
+
+        wynik.value = tekst;
+
+		przycisk_kopiuj.disabled = false;
+        return false;
+	}
+
+	przycisk_kopiuj.onclick = function()
+	{
+		navigator.clipboard.writeText(wynik.value);
+		var okienko = document.getElementById("okienko");
+		okienko.innerHTML = "Skopiowano!";
+
+		return false;
+	}
+};
+
+function outFunc() {
+	var okienko = document.getElementById("okienko");
+	okienko.innerHTML = "Kopiuj do schowka";
+}
